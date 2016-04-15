63bdcb914caf7b0e34e90040df1c7b58a1d3b35c
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-04-16 00:07:28 +0200

    fixed url with api

--- app/js/factory.js
+++ app/js/factory.js
@@ -6,13 +6,13 @@ angular.module('app.factory', [])
 
 // Create post factory to post data to api
 	codeFac.updateEntry = function(updateEntry){
-		
-		return	$http.post('http://boiling-mesa-13925.herokuapp.com//api/task-manager/update', updateEntry);
+
+		return	$http.post('http://boiling-mesa-13925.herokuapp.com/api/task-manager/update', updateEntry);
 	}
 
 // Create get factory to retrieve data from api
 	codeFac.all = function(){
-		return	$http.get('http://boiling-mesa-13925.herokuapp.com//api/task-manager');
+		return	$http.get('http://boiling-mesa-13925.herokuapp.com/api/task-manager');
 	}
 
 	return	codeFac;
