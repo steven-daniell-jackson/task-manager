c45145f12bb6ea2b631eec0944303247371ace0c
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-04-16 13:41:29 +0200

    fixed issue with api url

--- app/js/factory.js
+++ app/js/factory.js
@@ -7,18 +7,18 @@ angular.module('app.factory', [])
 // Create post factory to post data to api
 codeFac.updateEntry = function(updateEntry){
 	console.log(updateEntry);
-	return	$http.post('http://boiling-mesa-13925.herokuapp.com//api/task-manager/update', updateEntry);
+	return	$http.post('http://boiling-mesa-13925.herokuapp.com/api/task-manager/update', updateEntry);
 }
 
 // Create post factory to post data to api
 codeFac.archiveEntry = function(archiveEntry){
-	
-	return	$http.post('http://boiling-mesa-13925.herokuapp.com//api/task-manager/archive', archiveEntry);
+
+	return	$http.post('http://boiling-mesa-13925.herokuapp.com/api/task-manager/archive', archiveEntry);
 }
 	// Create post factory to post data to api
 	codeFac.deleteEntry = function(deleteEntry){
 		
-		return	$http.post('http://boiling-mesa-13925.herokuapp.com//api/task-manager/delete', deleteEntry);
+		return	$http.post('http://boiling-mesa-13925.herokuapp.com/api/task-manager/delete', deleteEntry);
 	}
 
 	// Create post factory to post data to api
@@ -32,12 +32,12 @@ codeFac.archiveEntry = function(archiveEntry){
 		};
 
 		// console.log(newEntryObj)
-		return	$http.post('http://boiling-mesa-13925.herokuapp.com//api/new-task-manager', newEntryObj);
+		return	$http.post('http://boiling-mesa-13925.herokuapp.com/api/new-task-manager', newEntryObj);
 	}
 
 // Create get factory to retrieve data from api
 codeFac.all = function(){
-	return	$http.get('http://boiling-mesa-13925.herokuapp.com//api/task-manager');
+	return	$http.get('http://boiling-mesa-13925.herokuapp.com/api/task-manager');
 }
 
 return	codeFac;
