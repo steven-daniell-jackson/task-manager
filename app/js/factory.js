3bb72bc997cd2066a51a174752850c48c6211a4f
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-04-16 13:40:05 +0200

    updated factory api locations

--- app/js/factory.js
+++ app/js/factory.js
@@ -5,42 +5,42 @@ angular.module('app.factory', [])
 	var codeFac = {};
 
 // Create post factory to post data to api
-	codeFac.updateEntry = function(updateEntry){
-console.log(updateEntry);
-		return	$http.post('http://localhost:3000/api/task-manager/update', updateEntry);
-	}
+codeFac.updateEntry = function(updateEntry){
+	console.log(updateEntry);
+	return	$http.post('http://boiling-mesa-13925.herokuapp.com//api/task-manager/update', updateEntry);
+}
 
 // Create post factory to post data to api
-	codeFac.archiveEntry = function(archiveEntry){
-		
-		return	$http.post('http://localhost:3000/api/task-manager/archive', archiveEntry);
-	}
+codeFac.archiveEntry = function(archiveEntry){
+	
+	return	$http.post('http://boiling-mesa-13925.herokuapp.com//api/task-manager/archive', archiveEntry);
+}
 	// Create post factory to post data to api
 	codeFac.deleteEntry = function(deleteEntry){
 		
-		return	$http.post('http://localhost:3000/api/task-manager/delete', deleteEntry);
+		return	$http.post('http://boiling-mesa-13925.herokuapp.com//api/task-manager/delete', deleteEntry);
 	}
 
 	// Create post factory to post data to api
 	codeFac.saveEntry = function(newEntry){
 
 
-var newEntryObj = {
-	'taskTitle' : newEntry,
-	'checked' : 'false',
-	'archived' : 'false'
-};
+		var newEntryObj = {
+			'taskTitle' : newEntry,
+			'checked' : 'false',
+			'archived' : 'false'
+		};
 
-		console.log(newEntryObj)
-		return	$http.post('http://localhost:3000/api/new-task-manager', newEntryObj);
+		// console.log(newEntryObj)
+		return	$http.post('http://boiling-mesa-13925.herokuapp.com//api/new-task-manager', newEntryObj);
 	}
 
 // Create get factory to retrieve data from api
-	codeFac.all = function(){
-		return	$http.get('http://localhost:3000/api/task-manager');
-	}
+codeFac.all = function(){
+	return	$http.get('http://boiling-mesa-13925.herokuapp.com//api/task-manager');
+}
 
-	return	codeFac;
+return	codeFac;
 
 })
 
