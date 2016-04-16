e274cc28c667390c2bef2a60c6d72f25d334f6aa
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-04-16 13:38:50 +0200

    Task manager functional

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,46 +1,78 @@
 angular.module('app.controllers',['app.factory'])
 
 // Main Controller
-  .controller('mainCtrl', ['$scope', 'TaskList',  function($scope, TaskList){
+.controller('mainCtrl', ['$scope', 'TaskList',  function($scope, TaskList){
+
+  $scope.archiveTog = false;
+
 
    // Access factory and assign the returned data
-TaskList.all()
-.success(function(data) {
-    
-            $scope.entries = data;
+   TaskList.all()
+   .success(function(data) {
+
+    $scope.entries = data;
             // $scope.preloader = false;
-           $scope.message = data.message;
-           console.log($scope.entries);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
+            $scope.message = data.message;
+            console.log($scope.entries);
+          })
+   .error(function(data) {
+    console.log('Error: ' + data);
+  });
 
-$scope.update = function(e){
 
-TaskList.updateEntry(this.entry);
- location.reload();
-  // console.log(this.entry);
+// Update entry function
+   $scope.update = function(e){
 
+    TaskList.updateEntry(this.entry);
+    location.reload();
 }
 
+
+// Delete entry function
 $scope.delete = function(){
+  if (confirm("Delete?") == true) {
   TaskList.deleteEntry(this.entry);
-   location.reload();
+  location.reload();
   console.log("delete");
+  }
 }
 
+// Archive entry function
 $scope.archive = function(){
 
-if (confirm("Archive?") == true) {
+  if (confirm("Archive?") == true) {
    TaskList.archiveEntry(this.entry);
    location.reload();
-  console.log("archived");
-}
-
+   console.log("archived");
+ }
  
 }
 
+// New entry function
+$scope.new = function(){
+var newEntry = prompt("New task");
+
+if (newEntry != "" && newEntry != null) {
+  TaskList.saveEntry(newEntry);
+    location.reload();
+} else {}
+   
+
+ }
+
+
+// Toggle Archived tasks
+ $scope.toggleArchive = function(e){
+
+  if($scope.archiveTog == "true") {
+    $scope.archiveTog = "false";
+  } else {
+    $scope.archiveTog = "true";
+  }
+
+
+
+}
 
 
 
@@ -53,7 +85,8 @@ if (confirm("Archive?") == true) {
 
 
 
-  }]);
-  
-  
-  
+
+
+}]);
+
+
--- app/js/factory.js
+++ app/js/factory.js
@@ -6,7 +6,7 @@ angular.module('app.factory', [])
 
 // Create post factory to post data to api
 	codeFac.updateEntry = function(updateEntry){
-
+console.log(updateEntry);
 		return	$http.post('http://localhost:3000/api/task-manager/update', updateEntry);
 	}
 
@@ -16,9 +16,23 @@ angular.module('app.factory', [])
 		return	$http.post('http://localhost:3000/api/task-manager/archive', archiveEntry);
 	}
 	// Create post factory to post data to api
-	codeFac.deleteEntry = function(archiveEntry){
+	codeFac.deleteEntry = function(deleteEntry){
 		
-		return	$http.post('http://localhost:3000/api/task-manager/delete', archiveEntry);
+		return	$http.post('http://localhost:3000/api/task-manager/delete', deleteEntry);
+	}
+
+	// Create post factory to post data to api
+	codeFac.saveEntry = function(newEntry){
+
+
+var newEntryObj = {
+	'taskTitle' : newEntry,
+	'checked' : 'false',
+	'archived' : 'false'
+};
+
+		console.log(newEntryObj)
+		return	$http.post('http://localhost:3000/api/new-task-manager', newEntryObj);
 	}
 
 // Create get factory to retrieve data from api
